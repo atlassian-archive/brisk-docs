445a3557036222dfdac83abb7707b891ffb6d715
Peter Yu <pyu@atlassian.com>
2019-03-13 15:24:47 +1100

    AFP-174: Mock navwrapper in jest test

--- packages/website/pages-list.js
+++ packages/website/pages-list.js
@@ -1,3 +1,9 @@
+/* pages-list.json is only generated upon building the website, and so will not exist
+   during linting. This file exists so there is a single place where pages-list.json is
+   imported so the eslint rule only needs to be disabled here, as opposed to every file
+   pages-list.json is used.
+*/
+
 /* eslint-disable import/no-unresolved */
 import pageInfo from './data/pages-list.json';
 /* eslint-disable import/no-unresolved */
