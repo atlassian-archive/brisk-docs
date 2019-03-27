b2ffd8aecaab4077eca08ea7ac773fe04c1bec99
Peter Yu <pyu@atlassian.com>
2019-03-27 16:34:48 +1100

    AFP-239: Throw error if DOCS_WEBSITE_CWD is not defined

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -7,7 +7,11 @@ const withCSS = require('@zeit/next-css');
 const handleConfig = require('./bin/handle-config');
 
 const configPath = process.env.DOCS_WEBSITE_CONFIG_PATH;
-const cwd = process.env.DOCS_WEBSITE_CWD || process.cwd();
+const cwd = process.env.DOCS_WEBSITE_CWD;
+
+if (!cwd) {
+  throw new Error('DOCS_WEBSITE_CWD is not defined');
+}
 
 const { webpack: clientWebpack } = handleConfig(cwd, configPath);
 
