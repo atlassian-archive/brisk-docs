0f1e820265f56e7b4ec57242a05eb6cd3516a66a
Aparna Raj <araj@atlassian.com>
2019-04-04 14:58:45 +1100

    AFP-246 intial commit to merge babel.configs

--- packages/website/babel.config.js
+++ packages/website/babel.config.js
@@ -1,4 +1,7 @@
+const cwd = process.env.DOCS_WEBSITE_CWD;
+
 module.exports = {
+  extends: `${cwd}/babel.config.js`, // to merge the consumer level babel.config
   presets: ['@babel/env', '@babel/preset-react', '@babel/preset-typescript'],
   plugins: [
     'emotion',
