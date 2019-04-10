6316f56b0fde330166e639f6ba514f96268ddb01
Peter Yu <pyu@atlassian.com>
2019-04-10 12:19:43 +1000

    AFP-248: Added changeset bot dependencies

--- packages/changeset-bot/index.js
+++ packages/changeset-bot/index.js
@@ -5,92 +5,107 @@ module.exports = app => {
   // Healthcheck
   const router = app.route('/');
   router.get('/healthcheck', (req, res) => {
-    res.send('OK')
+    res.send('OK');
   });
 
-
   app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' });
+    const issueComment = context.issue({
+      body: 'Thanks for opening this issue!',
+    });
     return context.github.issues.createComment(issueComment);
   });
 
   app.on('pull_request.opened', async context => {
     const params = context.issue();
 
-    const getFiles = context.github.pullRequests.listFiles(params)
-      .then((files) => {
-        const changesetFiles = files.data.filter((file) => file.filename.startsWith('.changeset'));
+    const getFiles = context.github.pullRequests
+      .listFiles(params)
+      .then(files => {
+        const changesetFiles = files.data.filter(file =>
+          file.filename.startsWith('.changeset'),
+        );
         return changesetFiles.length > 0;
       });
 
-    const getLatestCommit = context.github.pullRequests.listCommits(params)
-      .then((commits) => {
+    const getLatestCommit = context.github.pullRequests
+      .listCommits(params)
+      .then(commits => {
         return commits.data.pop();
       });
 
-    Promise.all([getFiles, getLatestCommit])
-      .then(([hasChangeset, latestCommit]) => {
+    Promise.all([getFiles, getLatestCommit]).then(
+      ([hasChangeset, latestCommit]) => {
         const user = latestCommit.committer.login;
         let prComment;
         if (!hasChangeset) {
-          prComment = context.issue(
-            {
-              body: `❌ NO CHANGESET PRESENT ❌.\nLatest commit: \`${user}\` committed \`${latestCommit.commit.message}\``
-            });
+          prComment = context.issue({
+            body: `❌ NO CHANGESET PRESENT ❌.\nLatest commit: \`${user}\` committed \`${
+              latestCommit.commit.message
+            }\``,
+          });
         } else {
-          prComment = context.issue(
-            {
-              body: `✅ This PR has a changeset ✅.\nLatest commit: \`${user}\` committed \`${latestCommit.commit.message}\``
-            });
+          prComment = context.issue({
+            body: `✅ This PR has a changeset ✅.\nLatest commit: \`${user}\` committed \`${
+              latestCommit.commit.message
+            }\``,
+          });
         }
 
         return context.github.issues.createComment(prComment);
-      });
+      },
+    );
   });
-
 
   app.on('pull_request.synchronize', async context => {
     const params = context.issue();
 
-    const getBotComment = context.github.issues.listComments(params)
-      .then((comments) => {
+    const getBotComment = context.github.issues
+      .listComments(params)
+      .then(comments => {
         const changesetBotComment = comments.data.filter(
-          (comment) => comment.user.login === 'changeset-bot[bot]'
+          comment => comment.user.login === 'changeset-bot[bot]',
         );
         const { id } = changesetBotComment[0];
         return id;
       });
 
-    const getFiles = context.github.pullRequests.listFiles(params)
-      .then((files) => {
-        const changesetFiles = files.data.filter((file) => file.filename.startsWith('.changeset'));
+    const getFiles = context.github.pullRequests
+      .listFiles(params)
+      .then(files => {
+        const changesetFiles = files.data.filter(file =>
+          file.filename.startsWith('.changeset'),
+        );
         return changesetFiles.length > 0;
       });
 
-    const getLatestCommit = context.github.pullRequests.listCommits(params)
-      .then((commits) => {
+    const getLatestCommit = context.github.pullRequests
+      .listCommits(params)
+      .then(commits => {
         return commits.data.pop();
       });
 
-    Promise.all([getBotComment, getFiles, getLatestCommit])
-      .then(([commentId, hasChangeset, latestCommit]) => {
+    Promise.all([getBotComment, getFiles, getLatestCommit]).then(
+      ([commentId, hasChangeset, latestCommit]) => {
         const user = latestCommit.committer.login;
         let prComment;
         if (!hasChangeset) {
-          prComment = context.issue(
-            {
-              comment_id: commentId,
-              body: `❌ NO CHANGESET PRESENT ❌.\nLatest commit: \`${user}\` committed \`${latestCommit.commit.message}\``
-            });
+          prComment = context.issue({
+            comment_id: commentId,
+            body: `❌ NO CHANGESET PRESENT ❌.\nLatest commit: \`${user}\` committed \`${
+              latestCommit.commit.message
+            }\``,
+          });
         } else {
-          prComment = context.issue(
-            {
-              comment_id: commentId,
-              body: `✅ This PR has a changeset ✅.\nLatest commit: \`${user}\` committed \`${latestCommit.commit.message}\``
-            });
+          prComment = context.issue({
+            comment_id: commentId,
+            body: `✅ This PR has a changeset ✅.\nLatest commit: \`${user}\` committed \`${
+              latestCommit.commit.message
+            }\``,
+          });
         }
 
         return context.github.issues.updateComment(prComment);
-      });
+      },
+    );
   });
 };
