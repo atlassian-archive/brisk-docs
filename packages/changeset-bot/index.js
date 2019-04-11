9cb00280e14cb40744fd4ad5d5bd8e030e9191fe
Peter Yu <pyu@atlassian.com>
2019-04-11 11:00:04 +1000

    AFP-248: Removed issues webhook handler

--- packages/changeset-bot/index.js
+++ packages/changeset-bot/index.js
@@ -8,13 +8,6 @@ module.exports = app => {
     res.send('OK');
   });
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({
-      body: 'Thanks for opening this issue!',
-    });
-    return context.github.issues.createComment(issueComment);
-  });
-
   app.on('pull_request.opened', async context => {
     const params = context.issue();
 
