f172a656bb2713d235aa24e26e3a0a59a6dc0d78
Ben Conolly <ben@thinkmill.com.au>
2019-04-26 16:20:25 +1000

    some performance hacks (#69)
    * Fell back on next's default configs to optimise webpack perf

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
-const preconstruct = require('preconstruct');
-
 module.exports = {
   setupFiles: ['./jest-setup.js'],
-  moduleNameMapper: preconstruct.aliases.jest(__dirname),
   testPathIgnorePatterns: ['./cypress/', '__fixtures__'],
   transformIgnorePatterns: ['node_modules/(?!(@atlaskit)/)'],
 };
