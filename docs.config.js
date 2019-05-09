f43e633742a863ba6d2ea45465212283de53e473
Ben Conolly <ben@thinkmill.com.au>
2019-05-09 19:28:08 +1000

    zzz bad mistkae

--- docs.config.js
+++ docs.config.js
@@ -1,9 +1,4 @@
 module.exports = () => ({
   siteName: 'Brisk Docs Docs',
   showSubExamples: true,
-  docs: {
-    path: './guides',
-    name: 'tangerine stuff',
-    description: 'why will things not just work why oh why?',
-  },
 });
