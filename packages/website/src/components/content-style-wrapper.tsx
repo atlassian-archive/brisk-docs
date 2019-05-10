f3f05e639a950605b7c411a47953c631f858bc28
Peter Yu <pyu@atlassian.com>
2019-05-10 12:09:21 +1000

    AFP-273: Set default container width to large and increase max-width

--- packages/website/src/components/content-style-wrapper.tsx
+++ packages/website/src/components/content-style-wrapper.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import { math, gridSize } from '@atlaskit/theme';
 
 export default styled.div`
-  max-width: ${math.multiply(gridSize, 85)}px;
+  max-width: ${math.multiply(gridSize, 130)}px;
+  padding: 0 ${math.multiply(gridSize, 5)}px;
   margin: ${math.multiply(gridSize, 5)}px auto;
 `;
