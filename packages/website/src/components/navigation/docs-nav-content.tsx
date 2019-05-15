46e7c016599c6816084d1a159a4ab46151c703a7
Aparna Raj <araj@atlassian.com>
2019-05-15 17:13:49 +1000

    Hide the back to home nav

--- packages/website/src/components/navigation/docs-nav-content.tsx
+++ packages/website/src/components/navigation/docs-nav-content.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { BackItem, MenuSection, Separator } from '@atlaskit/navigation-next';
+import { MenuSection } from '@atlaskit/navigation-next';
 import pageInfo from '../../pages-list';
-import LinkComponent from './link-component';
 import NavHeader from './nav-header';
 import TreeNavContent, { arrayToTreeItems } from './tree-nav-content';
 
@@ -23,8 +22,9 @@ const NavContent = () => {
         {/* TODO: TSFix nav typing */}
         {({ className }: { className: string }) => (
           <div className={className}>
-            <BackItem text="Back to home" href="/" component={LinkComponent} />
-            <Separator />
+            {/* TODO: Hiding for the time being */}
+            {/* <BackItem text="Back to home" href="/" component={LinkComponent} /> */}
+            {/* <Separator /> */}
             <TreeNavContent treeData={treeData} />
           </div>
         )}
