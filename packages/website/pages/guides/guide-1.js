0fc44785465cf728827de11a78f5a49162cf8482
Ben Conolly <ben@thinkmill.com.au>
2019-05-16 15:45:46 +1000

    ignore guides pages

--- packages/website/pages/guides/guide-1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Component from '../../../../guides/guide-1.md';
-import Wrapper from '../../src/components/page-templates/project-docs';
-
-export default () => (
-  <Wrapper data={{ key: 'guides', pageTitle: 'Guide 1' }}>
-    <Component />
-  </Wrapper>
-);
--- packages/website/pages/guides/sub-guides/guide-2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Component from '../../../../../guides/sub-guides/guide-2.md';
-import Wrapper from '../../../src/components/page-templates/project-docs';
-
-export default () => (
-  <Wrapper data={{ key: 'guides', pageTitle: 'Guide 2' }}>
-    <Component />
-  </Wrapper>
-);
