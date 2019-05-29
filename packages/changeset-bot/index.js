25950d651de67ad356cb1ed7af178b8ea5458f3f
Ben Conolly <ben@thinkmill.com.au>
2019-05-29 15:00:41 +1000

    bot now responds to remove changesets as a release

--- packages/changeset-bot/index.js
+++ packages/changeset-bot/index.js
@@ -1,20 +1,26 @@
 const getAbsentMessage = commitSha => `###  💥  No Changeset
 
-Latest commit: ${commitSha}
-
 Merging this PR will not cause any packages to be released. If these changes should not cause updates to packages in this repo, this is fine 🙂
 
 **If these changes should be published to npm, you need to add a changeset.**
 
-[Click here to learn what changesets are, and how to add one](https://github.com/Noviny/changesets/blob/master/docs/adding-a-changeset.md).`;
+[Click here to learn what changesets are, and how to add one](https://github.com/Noviny/changesets/blob/master/docs/adding-a-changeset.md).
+
+Latest commit: ${commitSha}`;
 
 const getApproveMessage = commitSha => `###  🦋  Changeset is good to go
 
-Latest commit: ${commitSha}
-
 **We got this.**
 
-Not sure what this means? [Click here  to learn what changesets are](https://github.com/Noviny/changesets/blob/master/docs/adding-a-changeset.md).`;
+Not sure what this means? [Click here  to learn what changesets are](https://github.com/Noviny/changesets/blob/master/docs/adding-a-changeset.md).
+
+Latest commit: ${commitSha}`;
+
+const getReleaseMessage = commitSha => `### 🦋🦋 Looks like this is a release branch
+
+There are no new changesets, but you've removed some. Happy publishing!
+
+Latest commit: ${commitSha}`;
 
 const getCommentId = (context, params) =>
   context.github.issues.listComments(params).then(comments => {
@@ -24,10 +30,19 @@ const getCommentId = (context, params) =>
     return changesetBotComment ? changesetBotComment.id : null;
   });
 
-const getChangesetId = (context, params) =>
+const checkAddedChangesets = (context, params) =>
   context.github.pullRequests.listFiles(params).then(files => {
     const changesetFiles = files.data.filter(
       file => file.filename.startsWith('.changeset') && file.status === 'added',
+    );
+    return changesetFiles.length > 0;
+  });
+
+const checkRemovedChangesets = (context, params) =>
+  context.github.pullRequests.listFiles(params).then(files => {
+    const changesetFiles = files.data.filter(
+      file =>
+        file.filename.startsWith('.changeset') && file.status === 'removed',
     );
     return changesetFiles.length > 0;
   });
@@ -51,15 +66,31 @@ module.exports = app => {
     const params = context.issue();
 
     const commentId = await getCommentId(context, params);
-    const hasChangeset = await getChangesetId(context, params);
+    const addedChangesets = await checkAddedChangesets(context, params);
     const latestCommit = await getLatestCommit(context, params);
+    const removedChangesets = await checkRemovedChangesets(context, params);
 
     let prComment;
-    if (!hasChangeset) {
+    /*
+      We have comments for 3 scenarios:
+      - no changesets added or removed
+      - no changesets added, some were removed
+      - changesets added
+
+      changesets added and removed falls through to 'changesets added' for now
+    */
+    if (!addedChangesets && !removedChangesets) {
       prComment = context.issue({
         comment_id: commentId,
         body: getAbsentMessage(latestCommit.sha),
       });
+    } else if (!addedChangesets && removedChangesets) {
+      prComment = context.issue({
+        comment_id: commentId,
+        body: getReleaseMessage(latestCommit.sha),
+      });
+
+      // We extrapolate this is a relase branch
     } else {
       prComment = context.issue({
         comment_id: commentId,
