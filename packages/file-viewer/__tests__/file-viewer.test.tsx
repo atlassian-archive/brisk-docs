c0addde66c618dc9d168f9a7526db1316875a760
Peter Yu <pyu@atlassian.com>
2019-05-29 11:33:12 +1000

    AFP-309: Added unit test for error case

--- packages/file-viewer/__tests__/file-viewer.test.tsx
+++ packages/file-viewer/__tests__/file-viewer.test.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import { mount, ReactWrapper } from 'enzyme';
 import ButtonExample from '../examples/example-files/atlaskit-button';
 import FileViewer from '../src/components/file-viewer';
+import ComponentWithError from './__fixtures__/component-with-error';
 
-describe('file viewer component', () => {
+describe('file viewer component normal render', () => {
   let wrapper: ReactWrapper;
   beforeEach(() => {
     wrapper = mount(
@@ -13,6 +14,10 @@ describe('file viewer component', () => {
         title="Button"
       />,
     );
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
   });
 
   it('should render file viewer component', () => {
@@ -34,5 +39,29 @@ describe('file viewer component', () => {
 
     expect(wrapper.find('Wrapper').prop('mode')).toBe('open');
     expect(wrapper.find('CodeStyle').exists()).toBe(true);
+  });
+});
+
+describe('file viewer with error in example component', () => {
+  it('should render the file viewer with a fall back error message', () => {
+    console.error = jest.fn();
+
+    const wrapper = mount(
+      <FileViewer
+        Component={ComponentWithError}
+        source="error component source"
+        title="Error component"
+      />,
+    );
+
+    expect(
+      wrapper
+        .find('ExampleShowcase')
+        .find('h4')
+        .text(),
+    ).toEqual('Something went wrong loading this example.');
+    expect(console.error).toHaveBeenCalled();
+
+    wrapper.unmount();
   });
 });
