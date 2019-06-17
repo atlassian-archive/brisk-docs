9b87d948c1fee7dde053a00cb9fa4e23956297ea
Ben Conolly <ben@thinkmill.com.au>
2019-06-17 15:34:28 +1000

    changeset

--- packages/website/__fixtures__/typescript-project/docs.config.js
+++ packages/website/__fixtures__/typescript-project/docs.config.js
@@ -1,25 +1,7 @@
 const path = require('path');
 
-// const webpack = config => {
-//   config.module.rules.push({
-//     test: new RegExp(`${__dirname}/.*\\.tsx$`),
-//     use: [
-//       {
-//         loader: 'babel-loader',
-//         options: {
-//           root: __dirname,
-//         },
-//       },
-//       'ts-loader',
-//     ],
-//   });
-
-//   return config;
-// };
-
 module.exports = () => ({
   packages: path.join(__dirname, 'typescript-package'),
   docs: { path: path.join(__dirname, 'docs'), name: 'docs' },
   siteName: 'Typescript/webpack Test Docs',
-  // webpack,
 });
