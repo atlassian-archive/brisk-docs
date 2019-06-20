3394e831897cd8b85462705e12103627adc7646b
Ben Conolly <ben@thinkmill.com.au>
2019-06-20 17:32:12 +1000

    a bit experimental

--- docs.config.js
+++ docs.config.js
@@ -1,19 +1,23 @@
+const docs = [
+  {
+    path: './docs',
+    name: 'Docs',
+    description:
+      'Information about brisk as a whole, and contributing to brisk',
+  },
+];
+
+if (process.env && process.env.BRANCH === 'noviny/better-docs') {
+  docs.push({
+    path: './example-pages',
+    name: 'Example Pages',
+    description: 'This is a collection of example pages for how pages ',
+  });
+}
+
 module.exports = () => ({
   siteName: 'Brisk Docs Docs',
   packages: ['./packages/*'],
   showSubExamples: true,
-  docs: [
-    {
-      path: './docs',
-      name: 'Docs',
-      description:
-        'Information about brisk as a whole, and contributing to brisk',
-    },
-    {
-      path: './test-pages',
-      name: 'Test Pages',
-      description:
-        "This is a collection of test pages used by brisk devs - you probably won't find anything useful in here",
-    },
-  ],
+  docs,
 });
