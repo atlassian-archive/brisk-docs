cf91bc57159e6cf438fa8991618cdff8cd36b0c6
Michael Blaszczyk <mblaszczyk@atlassian.com>
2019-06-24 17:13:51 +1000

    Fix more cypress tests

--- packages/website/cypress/integration/pages/docs-page.spec.js
+++ packages/website/cypress/integration/pages/docs-page.spec.js
@@ -1,8 +1,4 @@
 describe('Docs page tests', () => {
-  beforeEach(() => {
-    cy.visit('/docs/guides/how-to-be-accomplished');
-  });
-
   it('has the correct page title', () => {
     cy.visit('/docs/guides/nifty-tricks/staying-at-netherfield');
     cy.title().should('eq', 'Dummy Data Docs - Staying At Netherfield');
