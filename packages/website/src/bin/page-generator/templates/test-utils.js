331e4a8e2f61eb4433940f90ffb7b68068715350
Ben Conolly <ben@thinkmill.com.au>
2019-08-09 07:02:58 +1000

    removed old bin code

--- packages/website/src/bin/page-generator/templates/test-utils.js
+++
@@ -1,9 +0,0 @@
-const assertImportsReact = source => {
-  expect(source).toMatch(/^import React from 'react';$/m);
-};
-
-const assertValidTemplate = source => {
-  assertImportsReact(source);
-};
-
-export default assertValidTemplate;
