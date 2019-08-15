e3de2a2ea3e27a976ae0d96afff0aebf902132c5
Gora Kong <gora.kong93@gmail.com>
2019-08-15 16:24:23 -0700

    revert commit

--- packages/website/_app.js
+++ packages/website/_app.js
@@ -1,42 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from 'react-router-dom';
-import { asyncComponent } from 'react-async-component';
 
-const AsyncLoadPages = promise =>
-  asyncComponent({
-    resolve: () => promise,
-    LoadingComponent: () => <div>Loading</div>,
-    ErrorComponent: ({ error }) => (
-      <div>There is an error. {error.message} </div>
-    ),
-  });
+const HackyLoader = ({ promise }) => {
+  const [Comp, setComp] = useState(null);
+  promise.then(res => setComp(res));
+  if (Comp) return <Comp.default />;
+  return <div>Loading</div>;
+};
 
 export default () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={AsyncLoadPages(import('./pages'))} />
       <Route
-        path="/healthcheck"
-        component={AsyncLoadPages(import('./pages/healthcheck.tsx'))}
+        exact
+        path="/"
+        component={() => <HackyLoader promise={import('./pages')} />}
       />
-      <Route
-        path="/index"
-        component={AsyncLoadPages(import('./pages/index.tsx'))}
-      />
-      <Route
-        path="/packages"
-        component={AsyncLoadPages(import('./pages/packages.tsx'))}
-      />
-      <Route
-        path="/readme"
-        component={AsyncLoadPages(import('./pages/readme.js'))}
-      />
-      <Redirect to="/" />
     </Switch>
   </Router>
 );
