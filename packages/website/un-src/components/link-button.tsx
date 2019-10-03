d33ccb18ee9ce49d041ed7b2a762a94e03d5c066
Sarah Federman <sarahbethfederman@gmail.com>
2019-10-03 13:21:58 +1000

    fixed link button for gatsby

--- packages/website/un-src/components/link-button.tsx
+++ packages/website/un-src/components/link-button.tsx
@@ -9,25 +9,25 @@ the options available to you.
 import * as React from 'react';
 
 import Button from '@atlaskit/button';
-import Link from 'next/link';
+import LinkComponent from './navigation/link-component';
 
 /** Note: Render components need to be defined standalone. Defining them inline will create a
  * new function reference each time and cause re-mounts on each render. */
-const LinkButtonRenderComponent = ({
-  href,
-  children,
-  ...rest
-}: {
-  href: string;
-  children: React.ReactNode;
-}) => (
-  <Link href={href}>
-    <a {...rest}>{children}</a>
-  </Link>
-);
+// const LinkButtonRenderComponent = ({
+//   href,
+//   children,
+//   ...rest
+// }: {
+//   href: string;
+//   children: React.ReactNode;
+// }) => (
+//   <Link href={href}>
+//     <a {...rest}>{children}</a>
+//   </Link>
+// );
 
 const LinkButton = (props: typeof Button) => (
-  <Button {...props} component={LinkButtonRenderComponent} />
+  <Button {...props} component={LinkComponent} />
 );
 
 export default LinkButton;
