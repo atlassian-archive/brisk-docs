1280023f3312a433c0287b954fff5f72fcab4082
gwyneplaine <cc.lee@live.com.au>
2019-10-23 10:36:36 +1100

    debugging babel

--- packages/website/gatsby-node.js
+++ packages/website/gatsby-node.js
@@ -1,15 +1,11 @@
-const funAliases = require('../../../webite/relevant-file-name.js');
 exports.onCreateWebpackConfig = async ({
   resolve,
   actions
 }) => {
   actions.setWebpackConfig({
     resolve: {
-      mainFields: ['atlaskit:src', 'module', 'browser', 'main'],
+      // mainFields: ['atlaskit:src', 'module', 'browser', 'main'],
       extensions: ['.js', '.ts', '.tsx'],
-      alias: {
-        ...funaliases,
-      },
     }
   })
 }
