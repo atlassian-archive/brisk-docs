6b18a6a8099ae3366f1f8769a7a7cf0a57b7d203
Ben Conolly <ben@thinkmill.com.au>
2019-11-18 14:45:33 +1100

    massive moving files around thing

---
+++ website/docs.config.js
@@ -0,0 +1,39 @@
+const docs = [
+  {
+    path: '../docs',
+    urlPath: 'docs',
+    name: 'Docs',
+    description:
+      'Information about brisk as a whole, and contributing to brisk',
+  },
+];
+
+const links = [];
+
+if (process.env && process.env.CONTEXT !== 'production') {
+  docs.push({
+    path: '../example-pages',
+    urlPath: 'example-pages',
+    name: 'Example Pages',
+    description: 'This is a collection of example pages.',
+  });
+
+  links.push({
+    href: '/example-pages/guide-1',
+    label: 'Example guide 1',
+    description: 'This is an example arbitrary internal link.',
+  });
+  links.push({
+    href: 'https://atlaskit.atlassian.com',
+    label: 'Atlaskit',
+    description: 'This is an example arbitrary external link.',
+  });
+}
+
+module.exports = () => ({
+  siteName: 'Brisk Docs Docs',
+  packages: ['../packages/*'],
+  showSubExamples: false,
+  docs,
+  links,
+});
